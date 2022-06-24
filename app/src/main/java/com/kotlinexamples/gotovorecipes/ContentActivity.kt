7fbf2df2a2375fa0896c9a8ab6d52fcbc3cfd203
Levox98 <Levox98@users.noreply.github.com>
2022-06-24 20:08:52 +0500

    Start of work
    Drawer can now be shown from the full content activity

--- app/src/main/java/com/kotlinexamples/gotovorecipes/ContentActivity.kt
+++ app/src/main/java/com/kotlinexamples/gotovorecipes/ContentActivity.kt
@@ -2,20 +2,21 @@ package com.kotlinexamples.gotovorecipes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kotlinexamples.gotovorecipes.databinding.ContentLayoutBinding
 import com.kotlinexamples.gotovorecipes.databinding.FullContentLayoutBinding
 
 class ContentActivity : AppCompatActivity() {
 
-    lateinit var binding: FullContentLayoutBinding
+    lateinit var binding: ContentLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FullContentLayoutBinding.inflate(layoutInflater)
+        binding = ContentLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.idContentMainImage.setImageResource(intent.getIntExtra("image", R.drawable.ic_coffee))
-        binding.idContentName.text = intent.getStringExtra("title")
-        binding.idTvIngredients.text = intent.getStringExtra("ingredients")
-        binding.idContentRecipe.text = intent.getStringExtra("recipe")
+        binding.idFullContent.idContentMainImage.setImageResource(intent.getIntExtra("image", R.drawable.ic_coffee))
+        binding.idFullContent.idContentName.text = intent.getStringExtra("title")
+        binding.idFullContent.idTvIngredients.text = intent.getStringExtra("ingredients")
+        binding.idFullContent.idContentRecipe.text = intent.getStringExtra("recipe")
     }
 }
